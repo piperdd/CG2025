6b360d848a06348e4565b5c31760802c6508c199
piperdd <85449259+piperdd@users.noreply.github.com>
2025-10-05 10:12:25 +0100

    finished stroked triangles

--- Weekly Workbooks/03 Triangles and Textures/extras/TrianglesNTextures/src/TrianglesNTextures.cpp
+++ Weekly Workbooks/03 Triangles and Textures/extras/TrianglesNTextures/src/TrianglesNTextures.cpp
@@ -1,4 +1,6 @@
 #include <CanvasTriangle.h>
+#include <CanvasPoint.h>
+#include <Colour.h>
 #include <DrawingWindow.h>
 #include <Utils.h>
 #include <fstream>
@@ -6,6 +8,9 @@
 
 #define WIDTH 320
 #define HEIGHT 240
+
+#define STROKED 0
+#define FILLED 1
 
 std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
 	std::vector<float> result;
@@ -51,53 +56,64 @@ glm::vec3 barycentricToRGB(glm::vec3 pos) {
 	return pos * float(255);
 }
 
-void draw(DrawingWindow &window) {
-	window.clearPixels();
-	// we want to "standardise"/scale the values of 0-256 to the size of the window
-	// float scalingFactor = WIDTH/256
-	// input 319 -> output 255
-	// window max = 319
-	// window min = 0
-	// rgb max = 255
-	// rgb min = 0
-	// formula rgb = 319 / (319-0) * 255
+void line(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour) {
+	float xDiff = (to.x-from.x);
+	float yDiff = (to.y-from.y);
+	float numSteps = std::max(abs(xDiff), abs(yDiff));
+	float xStepSize = xDiff/numSteps;
+	float yStepSize = yDiff/numSteps;
+	// repeated addition instead of multiplication (out of bounds)
+	// float x = from.x;
+	// float y = from.y;
+	// for (int i = 0; i <= numSteps; i++) {
+	// 	x += xStepSize;
+	// 	y += yStepSize;
+	// 	uint32_t uintColour = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
+	// 	window.setPixelColour(int(x), int(y), uintColour);
+	// }
 
-	// std::vector<std::vector<glm::vec3>> canvas = colourInterpolation2D();
-	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			// RED NOISE
-			// float red = rand() % 256;
-			// float green = 0.0;
-			// float blue = 0.0;
-
-			// GREYSCALE INTERPOLATION
-			// float red = (window.width-x-1) * 255 / 319;
-			// float green = (window.width-x-1) * 255 / 319;
-			// float blue = (window.width-x-1) * 255 / 319;
-
-			// 2D COLOUR INTERPOLATION
-			// float red = canvas[y][x].x;
-			// float green = canvas[y][x].y;
-			// float blue = canvas[y][x].z;
-
-			// BARYCENTRIC TRIANGULAR INTERPOLATION
-			glm::vec2 v0(0, HEIGHT), v1(WIDTH/2, 0),v2(WIDTH, HEIGHT);
-			glm::vec3 pixelColour = barycentricToRGB(convertToBarycentricCoordinates(v0, v1, v2, glm::vec2(x,y)));
-			float minVal = std::min({pixelColour.x, pixelColour.y, pixelColour.z});
-
-			float red = 0;
-			float green = 0;
-			float blue = 0;
-			if (minVal >= 0) {
-				red = pixelColour.x;
-				green = pixelColour.y;
-				blue = pixelColour.z;
-			}
-
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
-		}
+	for (float i=0.0; i<=numSteps; i++) {
+		float x = from.x + xStepSize*i;
+		float y = from.y + yStepSize*i;
+		uint32_t uintColour = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
+		window.setPixelColour(round(x), round(y), uintColour);
 	}
+
+}
+
+void triangle(DrawingWindow &window, CanvasTriangle triangle, Colour colour, int type = STROKED) {
+	line(window, triangle.v0(), triangle.v1(), colour);
+	line(window, triangle.v1(), triangle.v2(), colour);
+	line(window, triangle.v2(), triangle.v0(), colour);
+}
+
+void randomTriangle (DrawingWindow &window, int type = STROKED) {
+	Colour randColour = Colour(rand() % 256, rand() % 256, rand() % 256);
+	CanvasPoint v0 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT);
+	CanvasPoint v1 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT);
+	CanvasPoint v2 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT);
+	CanvasTriangle randTriangle = CanvasTriangle(v0, v1, v2);
+	triangle(window, randTriangle, randColour);
+}
+
+
+
+void draw(DrawingWindow &window) {
+	// window.clearPixels();
+	//	Task 2: Line Drawing
+	CanvasPoint centre = CanvasPoint(WIDTH/2, HEIGHT/2);
+	CanvasPoint top = CanvasPoint(WIDTH/2, 0);
+	CanvasPoint bottom = CanvasPoint(WIDTH/2, HEIGHT-1);
+	CanvasPoint topLeft = CanvasPoint(0, 0);
+	CanvasPoint topRight = CanvasPoint(WIDTH-1, 0);
+	CanvasPoint middleLeftThird = CanvasPoint(WIDTH/3, HEIGHT/2);
+	CanvasPoint middleRightThird = CanvasPoint(2*WIDTH/3, HEIGHT/2);
+
+	Colour white = Colour(255,255,255);
+	line(window,  topLeft, centre, white);
+	line(window,  topRight, centre, white);
+	line(window,  top, bottom, white);
+	line(window,  middleLeftThird,  middleRightThird, white);
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -106,6 +122,7 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
 		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+		else if (event.key.keysym.sym == SDLK_u) randomTriangle(window);
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
 		window.saveBMP("output.bmp");
@@ -116,24 +133,10 @@ int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 
-	glm::vec3 from(1.0, 4.0, 9.2);
-	glm::vec3 to(4.0, 1.0, 9.8);
-	std::vector<glm::vec3> resultVec3;
-	resultVec3 = interpolateThreeElementValues(from, to, 4);
-	for(size_t i=0; i<resultVec3.size(); i++) {
-		std::cout << resultVec3[i].x << " " << resultVec3[i].y << " " << resultVec3[i].z << " ";
-		std::cout << std::endl;
-	}
-
-	std::vector<float> result;
-	result = interpolateSingleFloats(2.2, 8.5, 7);
-	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
-	std::cout << std::endl;
-
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+		// draw(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
