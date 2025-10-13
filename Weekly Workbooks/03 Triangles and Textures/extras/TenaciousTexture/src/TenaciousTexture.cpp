87d479cbcc9324cfe75cb536492862c9ed9e37eb
piperdd <85449259+piperdd@users.noreply.github.com>
2025-10-13 22:03:42 +0100

    finished workbook 3

--- Weekly Workbooks/03 Triangles and Textures/extras/TenaciousTexture/src/TenaciousTexture.cpp
+++ Weekly Workbooks/03 Triangles and Textures/extras/TenaciousTexture/src/TenaciousTexture.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <vector>
 
+#include "TextureMap.h"
+
 #define WIDTH 320
 #define HEIGHT 240
 
@@ -83,6 +85,48 @@ void line(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour
 		uint32_t uintColour = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
 		window.setPixelColour(round(x), round(y), uintColour);
 	}
+}
+
+std::vector<uint32_t> getTexturedLine(CanvasPoint from, CanvasPoint to, std::vector<std::vector<uint32_t>> texture) {
+	float xDiff = to.texturePoint.x-from.texturePoint.x;
+	float yDiff = to.texturePoint.y-from.texturePoint.y;
+	float numSteps = std::max(abs(xDiff), abs(yDiff));
+	std::vector<uint32_t> result;
+
+	if (numSteps == 0) {
+		return {texture[from.texturePoint.y][from.texturePoint.x]};
+	}
+	float xStepSize = xDiff/numSteps;
+	float yStepSize = yDiff/numSteps;
+	for (float i=0.0; i<=numSteps; i++) {
+		float x = from.texturePoint.x + xStepSize*i;
+		float y = from.texturePoint.y + yStepSize*i;
+		result.push_back(texture[y][x]);
+	}
+	return result;
+}
+
+void texturedLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, std::vector<std::vector<uint32_t>> texture) {
+	float xDiff = (to.x-from.x);
+	float yDiff = (to.y-from.y);
+	float numSteps = std::max(abs(xDiff), abs(yDiff));
+	if (numSteps == 0) {
+		uint32_t uintColour = texture[from.texturePoint.x][from.texturePoint.y];
+		window.setPixelColour(round(from.x), round(from.y), uintColour);
+		return;
+	}
+	float xStepSize = xDiff/numSteps;
+	float yStepSize = yDiff/numSteps;
+
+	std::vector<uint32_t> texturedLine = getTexturedLine(from, to, texture);
+	int texturedLineLength = texturedLine.size();
+	for (float i=0.0; i<=numSteps; i++) {
+		float x = from.x + xStepSize*i;
+		float y = from.y + yStepSize*i;
+		int index = texturedLineLength/numSteps * i;
+		uint32_t uintColour = texturedLine[index];
+		window.setPixelColour(round(x), round(y), uintColour);
+	}
 
 }
 
@@ -106,7 +150,13 @@ CanvasTriangle randomTriangle () {
 CanvasPoint pointOnLineY(float y, CanvasPoint p1, CanvasPoint p2) {
 	float proportion = (y - p1.y) / (p2.y - p1.y);
 	float x = proportion * (p2.x - p1.x) + p1.x;
-	return CanvasPoint(x, y);
+
+	CanvasPoint result = CanvasPoint(x, y);
+	float textureX = proportion * (p2.texturePoint.x-p1.texturePoint.x) + p1.texturePoint.x;
+	float textureY = proportion * (p2.texturePoint.y-p1.texturePoint.y) + p1.texturePoint.y;
+	result.texturePoint = TexturePoint(textureX, textureY);
+
+	return result;
 }
 
 std::vector<CanvasTriangle> splitTriangle(CanvasTriangle triangle) {
@@ -173,22 +223,49 @@ void drawRandomTriangle(DrawingWindow &window, int type = STROKED) {
 	}
 }
 
-void draw(DrawingWindow &window) {
-	// window.clearPixels();
-	//	Task 2: Line Drawing
-	CanvasPoint centre = CanvasPoint(WIDTH/2, HEIGHT/2);
-	CanvasPoint top = CanvasPoint(WIDTH/2, 0);
-	CanvasPoint bottom = CanvasPoint(WIDTH/2, HEIGHT-1);
-	CanvasPoint topLeft = CanvasPoint(0, 0);
-	CanvasPoint topRight = CanvasPoint(WIDTH-1, 0);
-	CanvasPoint middleLeftThird = CanvasPoint(WIDTH/3, HEIGHT/2);
-	CanvasPoint middleRightThird = CanvasPoint(2*WIDTH/3, HEIGHT/2);
+std::vector<std::vector<uint32_t>> loadTexture(std::string filename) {
+	TextureMap textureMap = TextureMap(filename);
+	int tMapHeight = textureMap.height;
+	int tMapWidth = textureMap.width;
+	std::vector<std::vector<uint32_t>> texture;
+	texture.resize(tMapHeight);
+	for (int i = 0; i < tMapHeight; i++) {
+		texture[i].resize(tMapWidth);
+	}
+	for (int i = 0; i < tMapHeight; i++) {
+		for (int j = 0; j <	tMapWidth; j++) {
+			texture[i][j] = textureMap.pixels[j + i*tMapWidth];
+		}
+	}
+	return texture;
+}
 
-	Colour white = Colour(255,255,255);
-	line(window,  topLeft, centre, white);
-	line(window,  topRight, centre, white);
-	line(window,  top, bottom, white);
-	line(window,  middleLeftThird,  middleRightThird, white);
+void drawTexturedTriangle(DrawingWindow &window, CanvasTriangle canvasTriangle, std::vector<std::vector<uint32_t>> texture) {
+	// input:
+	// canvas triangle
+	// texture triangle
+	// METHODOLOGY:
+	// split canvas triangle into two
+	// go row by row
+	// for each row, calculate each point that corresponds to the texture
+	CanvasTriangle topTriangle, bottomTriangle;
+	topTriangle = splitTriangle(canvasTriangle)[0];
+	bottomTriangle = splitTriangle(canvasTriangle)[1];
+
+	for (int y = topTriangle.v0().y; y < topTriangle.v1().y; y++) {
+		CanvasPoint p1 = pointOnLineY(y, topTriangle.v0(), topTriangle.v1());
+		CanvasPoint p2 = pointOnLineY(y, topTriangle.v0(), topTriangle.v2());
+		texturedLine(window, p1, p2, texture);
+	}
+
+	for (int y = bottomTriangle.v0().y; y < bottomTriangle.v2().y; y++) {
+		CanvasPoint p1 = pointOnLineY(y, bottomTriangle.v2(), bottomTriangle.v1());
+		CanvasPoint p2 = pointOnLineY(y, bottomTriangle.v2(), bottomTriangle.v0());
+
+		texturedLine(window, p1, p2, texture);
+	}
+	// draw white stroked triangle as outline
+	drawStrokedTriangle(window, canvasTriangle,Colour(255,255,255));
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -208,11 +285,21 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+	std::vector<std::vector<uint32_t>> texture = loadTexture("texture.ppm");
 
+	CanvasPoint v0 = CanvasPoint(160,10);
+	CanvasPoint v1 = CanvasPoint(300,230);
+	CanvasPoint v2 = CanvasPoint(10,150);
+	v0.texturePoint = TexturePoint(195,5);
+	v1.texturePoint = TexturePoint(395,380);
+	v2.texturePoint = TexturePoint(65,330);
+
+	CanvasTriangle canvasTriangle = CanvasTriangle(v0,v1,v2);
+
+	drawTexturedTriangle(window, canvasTriangle, texture);
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		// draw(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
